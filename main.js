337d2cbf546e6e81f699ad4eb37ecf916c614578
craccrock <103433512+craccrock@users.noreply.github.com>
2022-07-03 17:00:33 -0300

    heroku1

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import express from 'express'
 import { createClient, SchemaFieldTypes} from 'redis';
 
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 5000;
 
 app.use(express.static("public"));
 
